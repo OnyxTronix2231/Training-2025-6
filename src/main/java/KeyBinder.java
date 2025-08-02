5a13295ac58204a6ecc60c465d909fbf0dc85f0c
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2025-08-02 20:05:24 +0300

    key binder

---
+++ src/main/java/KeyBinder.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class KeyBinder {
+    private final char keyCh;
+    private final Runnable func;
+
+    public KeyBinder(char keyCh, Runnable func) {
+        this.keyCh = keyCh;
+        this.func = func;
+        setupFrame();
+    }
+
+    private void setupFrame() {
+        JFrame frame = new JFrame("KB");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(100, 100);
+        frame.setFocusable(true);
+
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                char key = e.getKeyChar();
+                if (key == keyCh && func != null) {
+                    func.run();
+                }
+            }
+        });
+
+        frame.setVisible(true);
+        frame.requestFocusInWindow();
+    }
+
+    public static void main(String[] args) {
+        KeyBinder keyBinder = new KeyBinder('a',
+                () -> {System.out.println("ploop");}
+        );
+    }
+}
