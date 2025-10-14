0ba09ffd2f193792b6e9d50af48c408d389dac5a
shakedweller-wq <shaked.weller@gmail.com>
2025-10-14 21:30:36 +0300

    shakked2

---
+++ src/main/java/L2/lecture/Shakked2.java
@@ -0,0 +1,25 @@
+package L2.lecture;
+
+public class Shakked2 {
+    public static void main(String[] args) {
+      int birth = 2010;
+
+      int today = 2025- birth;
+
+      int myBirth = today*365;
+        System.out.println("mybirth:" + myBirth);
+
+      int zvov = 17;
+      int zvovMe = myBirth/zvov;
+        System.out.println(zvovMe);
+
+      int life = 365%zvov; "לתקן"
+        System.out.println(life);
+
+      int myLife = myBirth/zvov;
+        System.out.println(myLife);
+
+
+
+    }
+}
