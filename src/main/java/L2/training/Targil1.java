0490e86f89f0f1fe1225c2c546552a09967d4e57
SagiNinyo <sagininyo@gmail.com>
2025-10-14 21:30:46 +0300

    Lesson 2

---
+++ src/main/java/L2/training/Targil1.java
@@ -0,0 +1,22 @@
+package L2.training;
+
+public class Targil1 {
+    public static void main(String[] args) {
+        int birthYear = 2010;
+        int Age = 2025 - birthYear;
+        int daysAlive = Age * 365;
+
+        int avgFly = 17;
+        int oldFromFly = daysAlive / avgFly;
+        int daysLeft = daysAlive % avgFly;
+        int deadFlies = avgFly - daysAlive % avgFly;
+
+        System.out.println("Your age is: " + Age);
+        System.out.println("The number od days in your life is: " + daysAlive);
+        System.out.println("You are" + oldFromFly + "times older than a fly");
+        System.out.println("The number of days left is: " + daysLeft);
+        System.out.println(deadFlies + "flies died during your lifetime");
+
+    }
+
+}
