9921f86992bae4675c286103177af3446a5776c6
Barel55 <noam888noam@gmail.com>
2025-10-15 19:02:38 +0300

    flies exercise

---
+++ src/main/java/L2/lecture/Idk2.java
@@ -0,0 +1,19 @@
+package L2.lecture;
+
+public class Idk2 {
+    public static void main(String[] args) {
+        int year = 2009;
+        int age = 2025-year;
+        int days = age*365;
+        double fly = days/17;
+        int time_left = 17 - days%17;
+        int x = days/17;
+        double d_year = 2009;
+        double d_age = 2025-d_year;
+        double d_days = d_age*365;
+        double d_fly = d_days/17;
+        double d_time_left = d_days%17;
+        double d_x = d_days%17;
+
+    }
+}
