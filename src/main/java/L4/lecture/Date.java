22c806a18517cc9dd9dde110b10e0cb26f5c6083
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2025-10-22 19:42:09 +0300

    sub date

--- src/main/java/L4/lecture/Date.java
+++ src/main/java/L4/lecture/Date.java
@@ -59,20 +59,70 @@ public class Date {
         }
     }
 
-    public int getDay() {
-        return day;
+    public static int getMonthNum(Month month) {
+        switch (month) {
+            case January:
+                return 1;
+            case February:
+                return 2;
+            case March:
+                return 3;
+            case April:
+                return 4;
+            case May:
+                return 5;
+            case June:
+                return 6;
+            case July:
+                return 7;
+            case August:
+                return 8;
+            case September:
+                return 9;
+            case October:
+                return 10;
+            case November:
+                return 11;
+            case December:
+                return 12;
+            default:
+                return 0;
+        }
     }
 
-    public void setDay(int day) {
-        this.day = clampDay(day, this.month);
+    public static Month getNumMonth(int num) {
+        switch (num) {
+            case 1:
+                return Month.January;
+            case 2:
+                return Month.February;
+            case 3:
+                return Month.March;
+            case 4:
+                return Month.April;
+            case 5:
+                return Month.May;
+            case 6:
+                return Month.June;
+            case 7:
+                return Month.July;
+            case 8:
+                return Month.August;
+            case 9:
+                return Month.September;
+            case 10:
+                return Month.October;
+            case 11:
+                return Month.November;
+            case 12:
+                return Month.December;
+            default:
+                return Month.January;
+        }
     }
 
-    public Month getMonth() {
-        return month;
-    }
-
-    public void setMonth(Month month) {
-        this.month = month;
+    public void satDate(){
+        System.out.println();
     }
 
     public int getYear() {
@@ -83,8 +133,27 @@ public class Date {
         this.year = year;
     }
 
+    public Month getMonth() {
+        return month;
+    }
+
+    public void setMonth(Month month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = clampDay(day, month);;
+    }
+
     public static void main(String[] args) {
         Date d = new Date(50, Month.February, 2026);
         System.out.println(d.getDay());
+        d.setDay(-15);
+        System.out.println(d.getDay());
+        System.out.println(d.getMonth());
     }
 }
