24f2c999d6b00008e7890a4085e1a75e90f19882
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2025-10-29 18:10:00 +0200

    l7 ready

---
+++ src/main/java/L7/lecture/Pet.java
@@ -0,0 +1,15 @@
+package L7.lecture;
+
+public class Pet {
+    protected String name;
+    protected int age;
+
+    public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void makeNoise() {
+        System.out.println(" ");
+    }
+}
