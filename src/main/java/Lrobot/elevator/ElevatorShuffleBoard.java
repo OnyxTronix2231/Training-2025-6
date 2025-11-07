d4a2fa85ef9a9bf77519969008ac3591f43a3fbf
SadAndRobotics <119526743+SadAndRobotics@users.noreply.github.com>
2025-11-07 21:15:03 +0200

    added pid calibration

--- src/main/java/Lrobot/elevator/ElevatorShuffleBoard.java
+++ src/main/java/Lrobot/elevator/ElevatorShuffleBoard.java
@@ -14,9 +14,17 @@ public class ElevatorShuffleBoard {
 
         GenericEntry dutyCycle = tab.add("target Val", 0).getEntry();
 
-
         tab.add("move elevator to position", new InstantCommand(() -> Elevator.getInstance().setWantedState(
-                Elevator.WantedState.MOVE_DUTY_CYCLE,dutyCycle.getDouble(0))));
+                Elevator.WantedState.MOVE_DUTY_CYCLE, dutyCycle.getDouble(0))));
+
+        GenericEntry targetLength = tab.add("target length", 0).getEntry();
+        tab.add("move elevator to position", new InstantCommand(() -> Elevator.getInstance().setWantedState(Elevator.WantedState.MOVE_TO_POSITION, targetLength.getDouble(0))));
+
+        GenericEntry pElevator = tab.add("p Elevator", 0).getEntry();
+        GenericEntry iElevator = tab.add("i Elevator", 0).getEntry();
+        GenericEntry dElevator = tab.add("d Elevator", 0).getEntry();
+        tab.add("update elevator pid", new InstantCommand(() -> Elevator.getInstance().updateElevatorPID(pElevator.getDouble(0), iElevator.getDouble(0), dElevator.getDouble(0))));
+        tab.addDouble("elevator err", () -> (targetLength.getDouble(0) - Elevator.getInstance().getElevatorLength()));
 
 
     }
