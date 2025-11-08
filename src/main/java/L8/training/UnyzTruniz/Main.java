927cc33474d9400d5cfb87050fb8300291abf524
Guy Sabban <h@ft.is-a.dev>
2025-11-08 19:17:45 +0200

    Update Main.java

--- src/main/java/L8/training/UnyzTruniz/Main.java
+++ src/main/java/L8/training/UnyzTruniz/Main.java
@@ -6,7 +6,6 @@ public class Main {
         Robot robot = new Robot(new Wheel(new NEO(), () -> 0.5), new Wheel(new Talon(), () -> 0.5), () -> Math.random());
         for (int i = 0; i < 1500; i++) {
             robot.periodic();
-//            TimeUnit.MILLISECONDS.sleep(100);
         }
     }
 }
