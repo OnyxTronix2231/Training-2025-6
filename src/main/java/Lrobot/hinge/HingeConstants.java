dd38a31dd668e80eb24008d645f7c6b88f3884ad
SagiNinyo <sagininyo@gmail.com>
2025-11-12 20:55:56 +0200

    Robot Training 2

---
+++ src/main/java/Lrobot/hinge/HingeConstants.java
@@ -0,0 +1,16 @@
+package Lrobot.hinge;
+
+import java.util.function.DoubleUnaryOperator;
+
+public class HingeConstants {
+
+    public static final int HINGE_MOTOR_ID = 9;
+
+    public static final double SIMULATION_HINGE_LENGTH_METERS = 0.001;
+    public static final double SIMULATION_HINGE_MASS_KG = 0.001;
+    public static final int SIMULATION_HINGE_NUM_OF_MOTORS = 2;
+
+    public static final double SIMULATION_DT_SECONDS = 0.02;
+
+}
+
---
+++ src/main/java/Lrobot/hinge/HingeIO.java
@@ -0,0 +1,16 @@
+package Lrobot.hinge;
+
+import frc.robot.lib.OnyxMotorInputs;
+
+public interface HingeIO {
+
+    void updateInputs(HingeInputs inputs);
+
+    class HingeInputs {
+
+        public OnyxMotorInputs hingeMotorInputs;
+    }
+
+    void setDutyCycle(double dutyCycle);
+}
+
