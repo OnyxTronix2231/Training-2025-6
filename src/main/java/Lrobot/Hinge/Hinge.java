80c5e5424d80d6ebc4b26d8721b07979ddf4ad31
Barel55 <noam888noam@gmail.com>
2025-11-15 20:34:51 +0200

    das

--- src/main/java/Lrobot/Hinge/Hinge.java
+++ src/main/java/Lrobot/Hinge/Hinge.java
@@ -1,5 +1,7 @@
 package Lrobot.Hinge;
 
+import TrainingUtils.KeyButton;
+import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.Superstructure;
 
@@ -10,24 +12,61 @@ public class Hinge extends SubsystemBase {
         OPEN,
         CLOSE
     }
+
     public enum WantedState {
         IDLING,
         OPENING,
         CLOSING
     }
+    private SystemState currentState;
+    private WantedState wantedState;
+    private HingeIO hingeIO;
 
-
+    private KeyButton button1;
+    private KeyButton button2;
 
     @Override
     public void periodic() {
-        super.periodic();
+        hingeIO.updateInputs();
+        updateWantedState();
+        currentState = handleStateTransition();
+        applyState();
 
+    }
+    public void updateWantedState() {
+        if (button1.isPressed()) {
+            wantedState = WantedState.OPENING;
+        }
+        else if (button2.isPressed()) {
+            wantedState = WantedState.CLOSING;
+        }
+        else {
+            wantedState = WantedState.IDLING;
+        }
     }
 
     public SystemState handleStateTransition () {
-        switch (WantedState) {
+        switch (wantedState) {
+            case IDLING:
+                return SystemState.IDLE;
+            case OPENING:
+                return SystemState.OPEN;
+            case CLOSING:
+                return SystemState.CLOSE;
+        }
+        return SystemState.IDLE;
+    }
+
+    public void applyState() {
+        switch (currentState) {
+            case OPEN:
+                hingeIO.setDutyCycle(0.1);
+            case CLOSE:
+                hingeIO.setDutyCycle(-0.1);
             case IDLE:
-                switch ()
+                hingeIO.setDutyCycle(0);
         }
     }
+
+
 }
