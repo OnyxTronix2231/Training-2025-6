bce6be3d924cf00d0dabd6c0b388cf700acdf083
Barel55 <noam888noam@gmail.com>
2025-11-16 10:39:22 +0200

    g

--- src/main/java/Lrobot/Hinge/Hinge.java
+++ src/main/java/Lrobot/Hinge/Hinge.java
@@ -1,72 +1,83 @@
 package Lrobot.Hinge;
 
 import TrainingUtils.KeyButton;
-import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.Superstructure;
 
 public class Hinge extends SubsystemBase {
 
     public enum SystemState {
-        IDLE,
-        OPEN,
-        CLOSE
-    }
-
-    public enum WantedState {
         IDLING,
         OPENING,
         CLOSING
     }
+
+    public enum WantedState {
+        IDLE,
+        OPEN,
+        CLOSE
+    }
     private SystemState currentState;
     private WantedState wantedState;
     private HingeIO hingeIO;
-
+    private HingeIO.HingeInputs hingeInputs;
     private KeyButton button1;
     private KeyButton button2;
 
+    public Hinge(HingeIO hingeIO) {
+        this.hingeIO = hingeIO;
+
+         hingeInputs = new HingeIO.HingeInputs();
+
+        this.hingeIO.updateInputs(hingeInputs);
+
+        wantedState = WantedState.IDLE;
+        currentState = SystemState.IDLING;
+        button1 = new KeyButton(1);
+        button2 = new KeyButton(2);
+    }
+
     @Override
     public void periodic() {
-        this.hingeIO.updateInputs(HingeIO.HingeInputs);
+        this.hingeIO.updateInputs(hingeInputs);
         updateWantedState();
         currentState = handleStateTransition();
         applyState();
-
     }
     public void updateWantedState() {
         if (button1.isPressed()) {
-            wantedState = WantedState.OPENING;
+            wantedState = WantedState.OPEN;
         }
         else if (button2.isPressed()) {
-            wantedState = WantedState.CLOSING;
+            wantedState = WantedState.CLOSE;
         }
         else {
-            wantedState = WantedState.IDLING;
+            wantedState = WantedState.IDLE;
         }
     }
 
     public SystemState handleStateTransition () {
         switch (wantedState) {
-            case IDLING:
-                return SystemState.IDLE;
-            case OPENING:
-                return SystemState.OPEN;
-            case CLOSING:
-                return SystemState.CLOSE;
+            case IDLE:
+                return SystemState.IDLING;
+            case OPEN:
+                return SystemState.OPENING;
+            case CLOSE:
+                return SystemState.CLOSING;
         }
-        return SystemState.IDLE;
+        return SystemState.IDLING;
     }
 
     public void applyState() {
         switch (currentState) {
-            case OPEN:
+            case OPENING:
                 hingeIO.setDutyCycle(0.1);
-            case CLOSE:
+                break;
+            case CLOSING:
                 hingeIO.setDutyCycle(-0.1);
-            case IDLE:
+                break;
+            case IDLING:
                 hingeIO.setDutyCycle(0);
+                break;
         }
     }
-
-
 }
